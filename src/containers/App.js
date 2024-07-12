e3b0f461c11a213ebc029b43f60cef8107d0a568
QuocHuy0808 <huy.quoc.nguyen@evizi.com>
2024-07-12 10:59:02 +0700

    sdsdsd

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1> Hello ReactJs! </h1>
         <a
           className="App-link"
